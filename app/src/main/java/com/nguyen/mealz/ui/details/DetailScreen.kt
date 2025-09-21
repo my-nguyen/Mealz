ef8ba3e218e55d5031d44a27a3d9bc5da3d4bc5b
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-09-20 19:32:51 -0700

    b11: Animate size of composable

--- app/src/main/java/com/nguyen/mealz/ui/details/DetailScreen.kt
+++ app/src/main/java/com/nguyen/mealz/ui/details/DetailScreen.kt
@@ -1,14 +1,22 @@
 package com.nguyen.mealz.ui.details
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -17,6 +25,9 @@ import com.nguyen.mealz.model.network.Category
 
 @Composable
 fun DetailsScreen(meal: Category?) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val imageSize: Dp by animateDpAsState(if (isExpanded) 200.dp else 100.dp)
+
     Column {
         Row {
             Card {
@@ -26,12 +37,21 @@ fun DetailsScreen(meal: Category?) {
                         .transformations(CircleCropTransformation())
                         .build(),
                     contentDescription = null,
-                    modifier = Modifier.size(200.dp)
+                    modifier = Modifier
+                        .size(imageSize)
+                        .padding(8.dp)
                 )
             }
-            Text(meal?.name ?: "default name")
+            Text(
+                meal?.name ?: "default name",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically)
+            )
         }
-        Button(onClick = { /*TODO*/ }) {
+        Button(modifier = Modifier.padding(16.dp), onClick = {
+            isExpanded = !isExpanded
+        }) {
             Text("Change picture state")
         }
     }

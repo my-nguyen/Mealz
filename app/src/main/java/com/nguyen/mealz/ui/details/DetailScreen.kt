9725ad6a031f411951c2939dafac8654c9387001
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-09-21 09:21:46 -0700

    b13: Collapsing toolbar on Column

--- app/src/main/java/com/nguyen/mealz/ui/details/DetailScreen.kt
+++ app/src/main/java/com/nguyen/mealz/ui/details/DetailScreen.kt
@@ -1,27 +1,25 @@
 package com.nguyen.mealz.ui.details
 
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.animateDp
-import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -30,47 +28,48 @@ import com.nguyen.mealz.model.network.Category
 
 @Composable
 fun DetailsScreen(meal: Category?) {
-    var pictureState by remember { mutableStateOf(PictureState.Normal) }
-    val transition = updateTransition(targetState = pictureState, label = "")
-    val size by transition.animateDp(label = "") { it.size }
-    val color by transition.animateColor(label = "") { it.color }
-    val borderWidth: Dp by transition.animateDp(label = "") { it.borderWidth }
+    val scrollState = rememberScrollState()
+    val offset = minOf(1f, 1 - (scrollState.value / 600f))
+    val size by animateDpAsState(targetValue = maxOf(100.dp, 200.dp * offset))
 
-    Column {
-        Row {
-            Card(
-                modifier = Modifier.padding(16.dp),
-                shape = CircleShape,
-                border = BorderStroke(borderWidth, color)
-            ) {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(meal?.imageUrl)
-                        .transformations(CircleCropTransformation())
-                        .build(),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(size)
-                        .padding(8.dp)
-                )
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column {
+            Surface(tonalElevation = 4.dp) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Card(
+                        modifier = Modifier.padding(16.dp),
+                        shape = CircleShape,
+                        border = BorderStroke(2.dp, Color.Green)
+                    ) {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data(meal?.imageUrl)
+                                .transformations(CircleCropTransformation())
+                                .build(),
+                            contentDescription = null,
+                            modifier = Modifier.size(size)
+                        )
+                    }
+                    Text(
+                        meal?.name ?: "default name",
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                }
             }
-            Text(
-                meal?.name ?: "default name",
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.CenterVertically)
-            )
-        }
-        Button(modifier = Modifier.padding(16.dp), onClick = {
-            pictureState =
-                if (pictureState == PictureState.Normal) PictureState.Expanded else PictureState.Normal
-        }) {
-            Text("Change picture state")
+            Column(modifier = Modifier.verticalScroll(scrollState)) {
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+                Text("This is a text element", modifier = Modifier.padding(32.dp))
+            }
         }
     }
-}
-
-enum class PictureState(val color: Color, val size: Dp, val borderWidth: Dp) {
-    Normal(Color.Magenta, 120.dp, 8.dp),
-    Expanded(Color.Green, 200.dp, 24.dp)
 }

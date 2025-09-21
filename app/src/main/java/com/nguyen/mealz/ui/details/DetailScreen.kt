2a10bbcd8a1584b72161d53761a48aae3ccbc4f3
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-09-20 19:46:22 -0700

    b12: Implementing expanding list items

--- app/src/main/java/com/nguyen/mealz/ui/details/DetailScreen.kt
+++ app/src/main/java/com/nguyen/mealz/ui/details/DetailScreen.kt
@@ -1,10 +1,14 @@
 package com.nguyen.mealz.ui.details
 
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -15,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -25,12 +30,19 @@ import com.nguyen.mealz.model.network.Category
 
 @Composable
 fun DetailsScreen(meal: Category?) {
-    var isExpanded by remember { mutableStateOf(false) }
-    val imageSize: Dp by animateDpAsState(if (isExpanded) 200.dp else 100.dp)
+    var pictureState by remember { mutableStateOf(PictureState.Normal) }
+    val transition = updateTransition(targetState = pictureState, label = "")
+    val size by transition.animateDp(label = "") { it.size }
+    val color by transition.animateColor(label = "") { it.color }
+    val borderWidth: Dp by transition.animateDp(label = "") { it.borderWidth }
 
     Column {
         Row {
-            Card {
+            Card(
+                modifier = Modifier.padding(16.dp),
+                shape = CircleShape,
+                border = BorderStroke(borderWidth, color)
+            ) {
                 AsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
                         .data(meal?.imageUrl)
@@ -38,7 +50,7 @@ fun DetailsScreen(meal: Category?) {
                         .build(),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(imageSize)
+                        .size(size)
                         .padding(8.dp)
                 )
             }
@@ -50,9 +62,15 @@ fun DetailsScreen(meal: Category?) {
             )
         }
         Button(modifier = Modifier.padding(16.dp), onClick = {
-            isExpanded = !isExpanded
+            pictureState =
+                if (pictureState == PictureState.Normal) PictureState.Expanded else PictureState.Normal
         }) {
             Text("Change picture state")
         }
     }
+}
+
+enum class PictureState(val color: Color, val size: Dp, val borderWidth: Dp) {
+    Normal(Color.Magenta, 120.dp, 8.dp),
+    Expanded(Color.Green, 200.dp, 24.dp)
 }
